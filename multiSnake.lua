f2583305d6e217a9b744d1eeda54a10caa3b2203
Matthew W <fatboychummy@gmail.com>
2019-11-25 22:59:28 -0700

    Snake body + collision detection

--- multiSnake.lua
+++ multiSnake.lua
@@ -8,20 +8,33 @@ local players = {}
 local apples = {}
 local wl = 'a'
 
+-- returns the position of all snake parts
+local function calcSnakePos(queue, tx, ty, x, y)
+  local ret = {{tx, ty}}
+
+  for i = 1, #queue - 1 do
+    tx, ty = etc.sim(tx, ty, queue[i])
+    ret[#ret + 1] = {tx, ty}
+  end
+
+  if x and y then
+    ret[#ret + 1] = {x, y}
+  end
+
+  return ret
+end
+
 -- loops through each player and moves them based on what direction they are
 -- facing
 local function move()
+  print("MOVE")
   for i = 1, #players do
     local player = players[i]
-    if player.dir == 0 then
-      player.y = player.y - 1
-    elseif player.dir == 2 then
-      player.y = player.y + 1
-    elseif player.dir == 1 then
-      player.x = player.x + 1
-    elseif player.dir == 3 then
-      player.x = player.x - 1
-    end
+    -- head
+    player.x, player.y = etc.sim(player.x, player.y, player.dir)
+
+    -- tail
+    player.tx, player.ty = etc.sim(player.tx, player.ty, player.tdir)
   end
 end
 
@@ -34,9 +47,24 @@ local function draw()
 
   for i = 1, #players do
     local player = players[i]
+    -- draw head
     term.setCursorPos(player.x, player.y)
     term.setBackgroundColor(colors.green)
     io.write(tostring(i))
+
+    -- draw tail
+    term.setCursorPos(player.tx, player.ty)
+    term.setBackgroundColor(colors.red)
+    io.write(' ')
+
+    -- draw snek
+    term.setBackgroundColor(colors.blue)
+    term.setCursorPos(player.tx, player.ty)
+    for j = 2, #player.tailQueue do
+      etc.moveCursor(player.tailQueue[j])
+      io.write(' ')
+      etc.moveCursor(3)
+    end
   end
   term.setBackgroundColor(colors.black)
 end
@@ -77,11 +105,49 @@ local function run()
       local player, dir = ev[2], ev[3]
 
       -- if the player is not trying to turn around...
-      if players[player].dir ~= (dir + 2) % 4 then
-        players[player].dir = dir
+      if players[player].ldir ~= (dir + 2) % 4 then
+        players[player].dir = dir -- allow the turn to complete
       end
     elseif ev[1] == "tick" then
-      -- TODO: tick handling
+      for i = 1, #players do
+        -- loop through each player, update their last direction
+        local player = players[i]
+        player.ldir = player.dir
+
+        -- loop through each player, check the tailQueue
+        table.remove(player.tailQueue, 1)
+        player.tdir = player.tailQueue[1]
+        player.tailQueue[#player.tailQueue + 1] = player.dir
+
+        -- loop through each player and check if we've crashed into them
+        -- including ourself
+        for j = 1, #players do
+          local player2 = players[j]
+          local ttx, tty = player2.x, player2.y
+
+          if player == player2 then
+            ttx = -100
+            tty = -100
+          end
+          local p2Pos = calcSnakePos(player2.tailQueue, player2.tx, player2.ty, ttx, tty)
+
+          -- for each position, check if the player's head is on this space
+          for k = 1, #p2Pos do
+            if player.x == p2Pos[k][1] and player.y == p2Pos[k][2] then
+              player.alive = false
+              term.setCursorPos(1, 1)
+              print("x:  ", player.x)
+              print("y:  ", player.y)
+              print("p2x:", p2Pos[k][1])
+              print("p2y:", p2Pos[k][2])
+              print("k:  ", k)
+              print("#p2:", #p2Pos)
+              --print("p2p:", textutils.serialize(p2Pos))
+              error("Player " .. tostring(i) .. " has died.", 0)
+            end
+          end
+        end
+      end
     end
   end
 end
@@ -94,7 +160,17 @@ local function main()
 
   -- create players
   for i = 1, plrs do
-    players[i] = {x = int.mul(mx, i / (plrs + 1)), y = int.div(my, 2), dir = 0}
+    players[i] = {
+      x = int.mul(mx, i / (plrs + 1)),
+      y = int.div(my, 2),
+      dir = 0,
+      ldir = 0,
+      tx = int.mul(mx, i / (plrs + 1)),
+      ty = int.div(my, 2) + 4,
+      tdir = 0,
+      tailQueue = {0, 0, 0, 0},
+      alive = true
+    }
   end
 
   -- launch game
@@ -106,8 +182,8 @@ local ok, err = pcall(main)
 
 term.setBackgroundColor(colors.black)
 term.setTextColor(colors.white)
-term.clear()
-term.setCursorPos(1, 1)
+--term.clear()
+--term.setCursorPos(1, 1)
 
 if not ok then
   printError(err)
